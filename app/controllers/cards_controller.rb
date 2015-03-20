5ff6c8dd70221487456795fc076b707324d9c0e3
cgardens <giardina.charles@gmail.com>
2015-03-20 11:40:57 -0700

    edit card works

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,12 @@
 class CardsController < ApplicationController
 
   def update
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
     @deck = @user.decks.find(params[:deck_id])
-    @card = @deck.cards.find(params[:card_id])
+    @card = @deck.cards.find(params[:id])
     @card.update_attributes(card_info)
 
-    render "/users/#{@user.id}/decks/#{@deck.id}/edit"
+    redirect_to user_deck_path(user_id: @user.id, id: @deck.id)
   end
 
   def new
@@ -14,7 +14,7 @@ class CardsController < ApplicationController
     @deck = @user.decks.find(params[:deck_id])
     @card = @deck.cards.new()
 
-    render "/users/#{@user.id}/decks/#{@deck.id}/edit"
+    # render "/users/#{@user.id}/decks/#{@deck.id}/edit"
   end
 
 
