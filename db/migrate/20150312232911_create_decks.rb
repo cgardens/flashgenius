1317103d9a92ba18f8673e2a6373bf9c7f53cf40
cgardens <giardina.charles@gmail.com>
2015-03-22 13:38:46 -0700

    displays correct timezone

--- db/migrate/20150312232911_create_decks.rb
+++ db/migrate/20150312232911_create_decks.rb
@@ -4,7 +4,7 @@ class CreateDecks < ActiveRecord::Migration
       t.string :user_id
       t.string :name
       t.float :performance_score
-      t.datetime :hour_mastery_is_attained
+      t.string :hour_mastery_is_attained
       t.string :current_mastery_level
       t.string :hours_until_deck_review
 
